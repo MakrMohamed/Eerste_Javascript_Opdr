ae3678d75fd55c6ff6f1f1addab8c9f7ffaf919b
MakrMohamed <84314376+MakrMohamed@users.noreply.github.com>
2021-06-08 14:23:20 +0200

    Add files via upload
    Eerste Javascript Opdracht

---
+++ index.js
@@ -0,0 +1,36 @@
+
+ console.log("Hello Winc Academy")
+ let name = 'Mohamed';
+ console.log(name);
+
+ let age1 = 4;
+ let age2 = 4;
+ console.log(age1 + age2);
+
+ let firstNumber = ('4')
+ let lastNumber = ('4');
+ console.log(firstNumber + lastNumber);
+
+
+ let y = 12;
+ let x = y + 12;
+ console.log(x);
+
+ let a = 20;
+ let b = a - 8;
+ console.log(b);
+
+ let c = 3;
+ let d = c * 3;
+ console.log(d);
+
+ let e = 2;
+ let f = e / 3;
+ console.log(f);
+
+ let g = 12;
+ let h = g % 50;
+ console.log(h);
+
+ let age = 31;
+ console.log(typeof 31);
